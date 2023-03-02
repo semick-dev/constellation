0515e72ff2f40976400a4df33be3bc46c9b9a71a
semick-dev <sbeddall@gmail.com>
2023-03-01 23:42:59 -0800

    ensure we update on linux installer too

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 dotnet pack --output=.install -c Release ./drone/downloader/Constellation.Drone.Downloader.csproj
-dotnet tool install -g --prerelease --add-source ./.install Constellation.Drone.Downloader
+dotnet tool update -g --prerelease --add-source ./.install Constellation.Drone.Downloader
 
